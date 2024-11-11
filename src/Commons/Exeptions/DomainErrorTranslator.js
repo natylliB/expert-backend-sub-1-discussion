eb5982a12663b7839966d85366894b21bc3115fe
BillyTan <www.billy39@gmail.com>
2024-11-11 20:45:20 +0700

    changed DomainErrorTranslator, now translate LoginCredential

--- src/Commons/Exeptions/DomainErrorTranslator.js
+++ src/Commons/Exeptions/DomainErrorTranslator.js
@@ -11,10 +11,10 @@ DomainErrorTranslator._directories = {
   'REGISTER_USER.PROPERTY_NOT_MET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai'),
   'REGISTER_USER.USERNAME_LIMIT_CHAR_EXCEEDED': new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'),
   'REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'),
-  'USER_CREDENTIAL.NOT_CONTAIN_REQUIRED_PROPERTY': new InvariantError('tidak dapat login karena properti yang dibutuhkan tidak ada'),
-  'USER_CREDENTIAL.PROPERTY_NOT_MET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat login karena tipe data tidak sesuai'),
-  'USER_CREDENTIAL.USERNAME_LIMIT_CHAR_EXCEEDED': new InvariantError('tidak dapat login karena karakter username melebihi batas limit'),
-  'USER_CREDENTIAL.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat login karena username mengandung karakter terlarang'),
+  'LOGIN_CREDENTIAL.NOT_CONTAIN_REQUIRED_PROPERTY': new InvariantError('tidak dapat login karena properti yang dibutuhkan tidak ada'),
+  'LOGIN_CREDENTIAL.PROPERTY_NOT_MET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat login karena tipe data tidak sesuai'),
+  'LOGIN_CREDENTIAL.USERNAME_LIMIT_CHAR_EXCEEDED': new InvariantError('tidak dapat login karena karakter username melebihi batas limit'),
+  'LOGIN_CREDENTIAL.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat login karena username mengandung karakter terlarang'),
 };
 
 module.exports = DomainErrorTranslator;
--- src/Commons/Exeptions/_test/DomainErrorTranslator.test.js
+++ src/Commons/Exeptions/_test/DomainErrorTranslator.test.js
@@ -11,13 +11,13 @@ describe('DomainErrorTranslator', () => {
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'));
     expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'));
-    expect(DomainErrorTranslator.translate(new Error('USER_CREDENTIAL.NOT_CONTAIN_REQUIRED_PROPERTY')))
+    expect(DomainErrorTranslator.translate(new Error('LOGIN_CREDENTIAL.NOT_CONTAIN_REQUIRED_PROPERTY')))
       .toStrictEqual(new InvariantError('tidak dapat login karena properti yang dibutuhkan tidak ada'));
-    expect(DomainErrorTranslator.translate(new Error('USER_CREDENTIAL.PROPERTY_NOT_MET_DATA_TYPE_SPECIFICATION')))
+    expect(DomainErrorTranslator.translate(new Error('LOGIN_CREDENTIAL.PROPERTY_NOT_MET_DATA_TYPE_SPECIFICATION')))
       .toStrictEqual(new InvariantError('tidak dapat login karena tipe data tidak sesuai'));
-    expect(DomainErrorTranslator.translate(new Error('USER_CREDENTIAL.USERNAME_LIMIT_CHAR_EXCEEDED')))
+    expect(DomainErrorTranslator.translate(new Error('LOGIN_CREDENTIAL.USERNAME_LIMIT_CHAR_EXCEEDED')))
       .toStrictEqual(new InvariantError('tidak dapat login karena karakter username melebihi batas limit'));
-    expect(DomainErrorTranslator.translate(new Error('USER_CREDENTIAL.USERNAME_CONTAIN_RESTRICTED_CHARACTER')))
+    expect(DomainErrorTranslator.translate(new Error('LOGIN_CREDENTIAL.USERNAME_CONTAIN_RESTRICTED_CHARACTER')))
       .toStrictEqual(new InvariantError('tidak dapat login karena username mengandung karakter terlarang'));
   })
 })
