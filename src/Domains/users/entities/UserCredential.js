d83bc2ff432bb84452a88f52220c2c559a805952
BillyTan <www.billy39@gmail.com>
2024-11-11 20:42:47 +0700

    changed the UserCredential, this object now used as repository return object

--- src/Domains/users/entities/UserCredential.js
+++ src/Domains/users/entities/UserCredential.js
@@ -1,25 +1,13 @@
 class UserCredential {
   constructor(payload) {
     this._verifyPayload(payload);
-
-    const { username, password } = payload;
-
-    this.username = username;
-    this.password = password;
   }
-
-  _verifyPayload({ username, password }) {
-    if (typeof username === 'undefined' || typeof password === 'undefined') {
+  _verifyPayload({ username, hashedPassword }) {
+    if (typeof username === 'undefined' || typeof hashedPassword === 'undefined') {
       throw new Error('USER_CREDENTIAL.NOT_CONTAIN_REQUIRED_PROPERTY');
     }
-    if (typeof username !== 'string' || typeof password !== 'string') {
+    if (typeof username !== 'string' || typeof hashedPassword !== 'string') {
       throw new Error('USER_CREDENTIAL.PROPERTY_NOT_MET_DATA_TYPE_SPECIFICATION');
-    }
-    if (username.length > 50) {
-      throw new Error('USER_CREDENTIAL.USERNAME_LIMIT_CHAR_EXCEEDED');
-    }
-    if (!username.match(/^[\w]+$/)) {
-      throw new Error('USER_CREDENTIAL.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
     }
   }
 }
--- src/Domains/users/entities/_test/UserCredential.test.js
+++ src/Domains/users/entities/_test/UserCredential.test.js
@@ -1,50 +1,21 @@
 const UserCredential = require('../UserCredential');
 
-describe('UserCredential object', () => {
-  it('should throw error when the property is missing', () => {
+describe('UserCredential({ username, hashedPassword }) object', () => {
+  it('should throw error when not contain required property', () => {
     const payload = {
-      username: 'abc',
-    }
+      username: 'billy',
+    };
 
     expect(() => new UserCredential(payload)).toThrow('USER_CREDENTIAL.NOT_CONTAIN_REQUIRED_PROPERTY');
   });
 
-  it('should throw error when the property not met data type specification', () => {
+  it('should throw error when property not met data type specification', () => {
     const payload = {
-      username: 'abcd',
-      password: {},
+      username: 'billy',
+      hashedPassword: 123,
     };
 
     expect(() => new UserCredential(payload)).toThrow('USER_CREDENTIAL.PROPERTY_NOT_MET_DATA_TYPE_SPECIFICATION');
   });
 
-  it('should throw error when the username is more than 50 character', () => {
-    const payload = {
-      username: 'somesuperlongusernamethatismorethan50charactersssss',
-      password: 'secret',
-    };
-
-    expect(() => new UserCredential(payload)).toThrow('USER_CREDENTIAL.USERNAME_LIMIT_CHAR_EXCEEDED');
-  });
-
-  it('should throw error when the username contain restricted character', () => {
-    const payload = {
-      username: '!!!billy',
-      password: 'secret',
-    };
-
-    expect(() => new UserCredential(payload)).toThrow('USER_CREDENTIAL.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
-  });
-
-  it('should create user credential object correctly', () => {
-    const payload = {
-      username: 'billy',
-      password: 'secret',
-    };
-
-    const { username, password } = new UserCredential(payload);
-
-    expect(username).toEqual(payload.username);
-    expect(password).toEqual(payload.password);
-  });
 });
