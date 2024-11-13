1d0054906322a0a65531b2ea8231b227e4e03452
BillyTan <www.billy39@gmail.com>
2024-11-13 21:34:31 +0700

    added compare function to BcryptPasswordHash

--- src/Infrastructures/security/_test/BcryptPasswordHash.test.js
+++ src/Infrastructures/security/_test/BcryptPasswordHash.test.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const BcryptPasswordHash = require('../BcryptPasswordHash');
+const AuthenticationError = require('../../../Commons/Exeptions/AuthenticationError');
 
 describe('BcryptPasswordHash', () => {
   describe('hash function', () => {
@@ -16,5 +17,27 @@ describe('BcryptPasswordHash', () => {
       expect(encryptedPassword).not.toEqual('plain_password');
       expect(spyHash).toHaveBeenCalledWith('plain_password', 10); // 10 adalah nilai salt round default untuk BcryptPasswordHash
     });
+  });
+
+  describe('compare function', () => {
+    it('should throw AuthenticationError when the password not match', async () => {
+      // Arrange
+      const spyCompare = jest.spyOn(bcrypt, 'compare').mockResolvedValue(false);
+      const bcryptPasswordHash =  new BcryptPasswordHash(bcrypt);
+
+      // Action & Assert
+      await expect(bcryptPasswordHash.compare('', '')).rejects.toThrow(new AuthenticationError('Password anda salah'));
+      expect(spyCompare).toHaveBeenCalledWith('', '');
+    });
+
+    it('should not throw AuthenticationError and successfully compare password when match', async () => {
+      // Arrange
+      const spyCompare = jest.spyOn(bcrypt, 'compare').mockResolvedValue(true);
+      const bcryptPasswordHash = new BcryptPasswordHash(bcrypt);
+
+      // Action & Assert
+      await expect(bcryptPasswordHash.compare('', '')).resolves.not.toThrow(new AuthenticationError);
+      expect(spyCompare).toHaveBeenCalledWith('', '');
+    })
   });
 });
